2d7abd71a34b2dbd14ecf9b36a4b95184c6e4546
Mohd Adnan <mohdadnan2320@gmail.com>
2025-02-21 17:36:56 +0530

    cors updated

--- backend/index.js
+++ backend/index.js
@@ -13,18 +13,40 @@ const app = express();
 //     credentials: true  
 // }));
 
+// const corsOptions = {
+//     origin: process.env.FRONTEND_URL,  // Make sure this matches the frontend URL
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Allow these methods
+//     allowedHeaders: ['Content-Type', 'Authorization'],  // Allow these headers
+//     credentials: true,  // Allow cookies if needed (for authorization)
+//   };
+
+// // Apply CORS middleware
+// app.use(cors(corsOptions));
+
+// // Handle preflight OPTIONS requests
+// app.options('*', cors(corsOptions));
+
+const allowedOrigins = process.env.FRONTEND_URL;
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+  });
+
 const corsOptions = {
-    origin: process.env.FRONTEND_URL,  // Make sure this matches the frontend URL
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Allow these methods
-    allowedHeaders: ['Content-Type', 'Authorization'],  // Allow these headers
-    credentials: true,  // Allow cookies if needed (for authorization)
-  };
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    credentials: true,
+    optionsSuccessStatus: 200 
+};
 
-// Apply CORS middleware
-app.use(cors(corsOptions));
-
-// Handle preflight OPTIONS requests
-app.options('*', cors(corsOptions));
+app.use(cors(corsOptions))
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
