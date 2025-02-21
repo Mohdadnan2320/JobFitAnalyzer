c54bec70c3faf16f676192b2d44294403d1ece9d
Mohd Adnan <mohdadnan2320@gmail.com>
2025-02-21 15:20:20 +0530

    Updates component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import UploadCard from "./components/UploadCard";
-import JobList from "./components/jobList";
-
+import ListOfJobs from "./components/ListOfJobs";
 const App = () => {
   return (
     <>
     <Routes>
     <Route path="/" element={<UploadCard />} />    
-    <Route path="/jobs" element={<JobList />} />    
+    <Route path="/jobs" element={<ListOfJobs />} />    
     </Routes>
     </>
   );
