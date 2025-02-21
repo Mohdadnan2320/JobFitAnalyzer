4ac3042fa2499a0454fd4ecd994c1a77ba04e79d
Mohd Adnan <mohdadnan2320@gmail.com>
2025-02-21 17:17:01 +0530

    cors updated

--- backend/index.js
+++ backend/index.js
@@ -6,12 +6,25 @@ const { connectDB } = require('./db/db');
 const uploadRoute = require('./routes/uploadRoute')
 const app = express();
 
-app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'], 
-    credentials: true  
-}));
+// app.use(cors({
+//     origin: process.env.FRONTEND_URL,
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type', 'Authorization'], 
+//     credentials: true  
+// }));
+
+const corsOptions = {
+    origin: process.env.FRONTEND_URL,  // Make sure this matches the frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Allow these methods
+    allowedHeaders: ['Content-Type', 'Authorization'],  // Allow these headers
+    credentials: true,  // Allow cookies if needed (for authorization)
+  };
+
+// Apply CORS middleware
+app.use(cors(corsOptions));
+
+// Handle preflight OPTIONS requests
+app.options('*', cors(corsOptions));
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
